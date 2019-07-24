15fb94c6e3bf0cbe9f99994983955b6c2ab3c0a7
Logan Darklock <logandarklock@gmail.com>
2019-07-24 15:29:11 -0700

    Version bump

--- src/main/java/net/logandark/mods/diaronlapiredcoalgold/Constants.java
+++ src/main/java/net/logandark/mods/diaronlapiredcoalgold/Constants.java
@@ -3,5 +3,5 @@ package net.logandark.mods.diaronlapiredcoalgold;
 public class Constants {
 	public static final String modid = "diaronlapiredcoalgold";
 	static final String modname = "Diaronlapiredcoalgold Ore";
-	static final String version = "1.0";
+	static final String version = "1.0.1";
 }
